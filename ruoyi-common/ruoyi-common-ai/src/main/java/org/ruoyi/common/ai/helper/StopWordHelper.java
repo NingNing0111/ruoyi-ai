79e73c92f326aca05ab5b5f6c4c67220d646e9cd
NingNing0111 <zdncode@gmail.com>
2025-06-20 16:22:27 +0800

    StringHelper

---
+++ ruoyi-common/ruoyi-common-ai/src/main/java/org/ruoyi/common/ai/helper/StopWordHelper.java
@@ -0,0 +1,18 @@
+package org.ruoyi.common.ai.helper;
+
+/**
+ * @author Zhang De Ning
+ * @email zhangdening@huice.com
+ * @time 2025-06-20 16:07
+ * @description 停止词处理
+ */
+public class StopWordHelper extends StringHelper{
+     public StopWordHelper(String content) {
+        super(content);
+    }
+
+    @Override
+    public String parse() {
+        return "";
+    }
+}
---
+++ ruoyi-common/ruoyi-common-ai/src/main/java/org/ruoyi/common/ai/vector/client/VectorDBClient.java
@@ -0,0 +1,11 @@
+package org.ruoyi.common.ai.vector.client;
+
+/**
+ * @author Zhang De Ning
+ * @email zhangdening@huice.com
+ * @time 2025-06-20 14:10
+ * @description
+ */
+public interface VectorDBClient<T> {
+
+}
